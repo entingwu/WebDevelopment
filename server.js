3ba3c11c0f7964fe8af9370e3bb4ebba615cd1ea
Enting  Wu <entingwu@ZHIDONGs-MacBook-Pro.local>
2016-01-22 00:32:04 -0800

    Replaced server.js with hello world message

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(express.static(__dirname + '/public'));
  var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
  var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.get('/helloWorld',function(req,res) {
- res.send('hello world still me!');
+app.get('/hello', function(req, res){
+ res.send('hello world');
 });
 
 app.listen(port, ipaddress);
